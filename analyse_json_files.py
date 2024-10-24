d0e52c3a8d42780714c8f77f2e43d73da48c7ee6
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-10-24 22:21:22 +0200

    added date handling

--- analyse_json_files.py
+++ analyse_json_files.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime as dt
 
 file = "recently_played.json"
 
@@ -23,14 +24,25 @@ for item in data["items"]:
     release_date = album["release_date"]
     album_id = album["id"]
 
-    artiste = track["artists"]
-    # https://api.spotify.com/v1/artists/
+    artistes = track["artists"]
+    artistes_du_morceau = []
+    for artiste in artistes:
+        # print(artiste)
+        # https://api.spotify.com/v1/artists/
 
-    # Interesting data
-    artiste_name = artiste["name"]
-    artiste_id = artiste["id"]
-
+        # Interesting data
+        artiste_name = artiste["name"]
+        artistes_du_morceau.append(artiste_name)
+        artiste_id = artiste["id"]
+        artistes_du_morceau.append(artiste_id)
+    # print(artistes_du_morceau)
 
     # Data on myself
     played_at = item["played_at"]
-    print(played_at.type())
+    # print(played_at.type())
+
+after_date = str({data["cursors"]["after"]})[2:-2]
+before_date = str({data["cursors"]["before"]})[2:-2]
+
+print(f"after = {dt.fromtimestamp(int(after_date)/1000).strftime('%Y-%m-%d %H:%M:%S')}")
+print(f"before = {dt.fromtimestamp(int(before_date)/1000).strftime('%Y-%m-%d %H:%M:%S')}")
